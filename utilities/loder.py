312e71880f0daee61382f0a21336f39535587d6a
Chen <zdms1987@hotmail.com>
2019-05-29 12:05:22 -0500

    use the logger to handle errors
    the logger will take care of the errors

--- utilities/loder.py
+++ utilities/loder.py
@@ -7,13 +7,19 @@ Common file loader functions.
 
 import yaml
 
+from tomoproc.utilities.logger import log_exception
+from tomoproc.utilities.logger import logger_default
+
+
+@log_exception(logger_default)
 def load_yaml(fname: str) -> dict:
     """generic safe_load wrapper for yaml archive"""
-    try:
-        with open(fname, 'r') as f:
-            dataMap = yaml.safe_load(f)
-    except IOError as e:
-        print(f"Cannot open YAML file {fname}")
-        print(f"IOError: {e}")
-    
+    # NOTE: error handeling is done via logger
+    with open(fname, 'r') as f:
+        dataMap = yaml.safe_load(f)
+
     return dataMap
+
+
+if __name__ == "__main__":
+    pass
