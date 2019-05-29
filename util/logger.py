77c4c94439aaa50cb71344404519d2d06e738b51
Chen <zdms1987@hotmail.com>
2019-05-29 15:37:36 -0500

    make create_logger() func more useful
    also add new log_event decorator

--- util/logger.py
+++ util/logger.py
@@ -9,13 +9,13 @@ import functools
 import logging
 
 
-def create_logger():
+def create_logger(logfile=r"/tmp/tomoproc.log"):
     """Default logger for exception tracking"""
     logger = logging.getLogger("tomoproc_logger")
     logger.setLevel(logging.INFO)
 
     # create the logging file handler
-    fh = logging.FileHandler(r"/tmp/tomoproc.log")
+    fh = logging.FileHandler(logfile)
     fh.setFormatter(
          logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -31,9 +31,7 @@ logger_default = create_logger()
 
 def log_exception(logger):
     """decorator for logging exception"""
- 
     def decorator(func):
- 
         def wrapper(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
@@ -46,11 +44,22 @@ def log_exception(logger):
                 # re-raise the exception
                 raise
         return wrapper
-
     return decorator
 
 
-@log_exception(logger_default)
+def log_event(logger):
+    """decorator for verbose event logging"""
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            args_str = ",".join(map(str, args))
+            kwargs_str = ",".join([f"{k}={v}" for k,v in kwargs.items()])
+            logger.info(f'Executing {func.__name__}()\n\targs: {args_str}\n\tkwargs:{kwargs_str}')
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+# @log_exception(logger_default)
+@log_event(logger_default)
 def _test_logger(a, b=0, c=1):
     return a/b
 
