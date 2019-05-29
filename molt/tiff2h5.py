515a29883f1e5ff35beddd16e217a5b0602ac271
Chen <zdms1987@hotmail.com>
2019-05-29 13:38:26 -0500

    Create tiff2h5.py

---
+++ molt/tiff2h5.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Using the experiment config file (YAML) to convert tiff images to a single
+HDF5 archive that has the same structure from direct output of area detector.
+"""
+
+import h5py
+from   tomoproc.util.loder import load_yaml
+
+
+if __name__ == "__main__":
+    pass
