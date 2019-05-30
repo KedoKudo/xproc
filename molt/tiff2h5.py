d34b458c6e85ab0809078bf8cd6d31a27c261637
Chen <zdms1987@hotmail.com>
2019-05-30 13:51:19 -0500

    fix various bugs found during test
    the convertor should work now

--- molt/tiff2h5.py
+++ molt/tiff2h5.py
@@ -8,6 +8,7 @@ HDF5 archive that has the same structure from direct output of area detector.
 
 import os
 import h5py
+import numpy                as     np
 from   tifffile             import imread
 from   tomoproc.util.file   import load_yaml
 from   tomoproc.util.file   import recursive_save_dict_to_h5group
@@ -15,7 +16,7 @@ from   tomoproc.util.logger import logger_default
 from   tomoproc.util.logger import log_exception
 
 
-@log_exception(logger_default)
+# @log_exception(logger_default)
 def pack_tiff_to_hdf5(fconfig:  str) -> None:
     """
     Description
@@ -45,35 +46,32 @@ def pack_tiff_to_hdf5(fconfig:  str) -> None:
     fnpre = cfg['file_prefix']
     ffmt  = cfg['file_format']
 
-    # craete HDF5 file handle
-    h5f = h5py.File(os.path.join(fpath, f"{fnpre}.h5"))
+    with h5py.File(os.path.join(fpath, f"{fnpre}.h5")) as h5f:
+        # write the configuration
+        recursive_save_dict_to_h5group(h5f, '/config/', cfg_all)
 
-    # write the configuration
-    recursive_save_dict_to_h5group(h5f, '/config', cfg_all)
+        # get example image
+        _padding = cfg['numpadding']
+        _fn = f"{fnpre}_{str(cfg['front_white'][0]).zfill(_padding)}.{ffmt}"
+        _img = imread(os.path.join(fpath, _fn))
+        _nrow, _ncol = _img.shape
+        _dtype = _img.dtype
 
-    # get example image
-    _padding = cfg['numpadding']
-    _fn = f"{fnpre}_{str(cfg['front_white'][0]).zfill(_padding)}.{ffmt}"
-    _img = imread(os.path.join(fpath, _fn))
-    _nrow, _ncol = _img.shape
-    _dtype = _img.dtype
-
-    # write four types of img stack to h5 archive
-    for k, v in {
-        'front_white': '/exchange/data_white_pre',
-        'projections': '/exchange/data',
-        'back_white' : '/exchange/data_white_post',
-        'back_dark'  : '/exchange/data_white_dark',
-        }:
-        _nimg = cfg[k][1] - cfg[k][0] + 1
-        _dst = h5f.create_dataset(v, (_nimg, _nrow, _ncol), dtype=_dtype)
-        for n in range(cfg[k][0],  cfg[k][1]+1):
-            _fn = f"{fnpre}_{str(n).zfill(_padding)}.{ffmt}"
-            _dst[n,:,:] = imread(os.path.join(fpath, _fn))
-    
-    # close file
-    h5f.close()
+        # write four types of img stack to h5 archive
+        for k, v in {
+            'front_white': '/exchange/data_white_pre',
+            'projections': '/exchange/data',
+            'back_white' : '/exchange/data_white_post',
+            'back_dark'  : '/exchange/data_white_dark',
+            }.items():
+            _nimgs = np.arange(cfg[k][0], cfg[k][1] + 1)
+            print(len(_nimgs), _nrow, _ncol)
+            _dst = h5f.create_dataset(v, (len(_nimgs), _nrow, _ncol), dtype=_dtype)
+            for idx, n in enumerate(_nimgs):
+                _fn = f"{fnpre}_{str(n).zfill(_padding)}.{ffmt}"
+                _dst[idx,:,:] = imread(os.path.join(fpath, _fn))
 
 
 if __name__ == "__main__":
-    pass
+    import sys
+    pack_tiff_to_hdf5(sys.argv[1])
