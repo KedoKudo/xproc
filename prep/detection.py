eff3db7a3e8536b0c8644f82282a3bc4bf325825
Chen <zdms1987@hotmail.com>
2019-06-13 15:20:07 -0500

    add corrupted frame detection function

--- prep/detection.py
+++ prep/detection.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 """
 Provide functions that operate on projections
 """
 
-from   typing               import Tuple
 import numpy                as     np
+
+from   typing               import Tuple
+from   scipy.signal         import medfilt
 from   scipy.signal         import medfilt2d
 from   scipy.ndimage        import gaussian_filter
 from   tomopy               import minus_log
 from   tomopy               import find_center_pc
+from   tomoproc.util.npmath import rescale_image
 from   tomoproc.util.logger import logger_default
 from   tomoproc.util.logger import log_exception
 
@@ -63,22 +67,50 @@ def detect_sample_in_sinogram(
 
 def detect_corrupted_proj(
     projs: np.ndarray,
-    thetas: np.ndarray,
+    omegas: np.ndarray,
     threshold: float=0.05,
     ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Description
     -----------
-    The corrupted (bad) frames 
+    Corrupted frames/projections will add a forgy layer (artifact) of random 
+    noise to the final reconstruction.  These corrupted frames can be detected
+    through 180 degree pair-wise checking.
+
     Parameters
     ----------
+    projs: np.ndarray
+        tomography image stack [axis_omega, axis_imgrow, axis_imgcol]
+    omegas: np.ndarray
+        angular position vector
+    threshold: float
+        Threshold for picking out the outliers
 
     Returns
     -------
     tuple(np.ndarray, np.ndarray)
-        Return the indices of bad frames and good frames
+           idx_BAD     idx_GOOD
+        Return the indices of BAD frames and GOOD frames/projections
     """
-    pass
+    # assume equal step, find the index range equals to 180 degree
+    dn = int(np.pi/(omegas[1] - omegas[0]))
+
+    # get the cnts from each 180 pairs
+    cnts = [
+        tomopy.find_center_pc(
+            rescale_image(minus_log(projs[n_img,:,:])), 
+            rescale_image(minus_log(projs[n_img+dn,:,:])), 
+            rotc_guess=projs.shape[2]/2,
+            )   for nimg in range(dn)
+    ]
+
+    # 180 -> 360
+    cnts = np.array(cnts + cnts)
+
+    # locate outlier
+    diff = np.absolute(cnts - medfilt(cnts))/cnts
+
+    return np.where(diff>threshold)[0], np.where(diff<=threshold)[0]
 
 
 if __name__ == "__main__":
