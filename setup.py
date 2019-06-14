08619a1f6021366d8efe7666c99eb462ed5935f9
Chen <zdms1987@hotmail.com>
2019-06-14 15:31:08 -0500

    adjust setup configuration

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='tomoproc',  
+     version='0.0.1',
+     scripts=['tomoproc'] ,
+     author="KedoKudo",
+     author_email="chenzhang8722@gmail.com",
+     description="Automated tomography reconstruction meta-package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/KedoKudo/tomoproc.git",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
