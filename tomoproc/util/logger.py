d1bd23029768420360b23f14b8bcf7a0a9b9a5d3
Chen <zdms1987@hotmail.com>
2019-09-04 15:40:20 -0500

    use wrap to provide correct meta info

--- tomoproc/util/logger.py
+++ tomoproc/util/logger.py
@@ -32,6 +32,7 @@ logger_default = create_logger()
 def log_exception(logger):
     """decorator for logging exception"""
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
@@ -50,6 +51,7 @@ def log_exception(logger):
 def log_event(logger):
     """decorator for verbose event logging"""
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             args_str = ",".join(map(str, args))
             kwargs_str = ",".join([f"{k}={v}" for k,v in kwargs.items()])
@@ -58,11 +60,15 @@ def log_event(logger):
         return wrapper
     return decorator
 
-# @log_exception(logger_default)
-@log_event(logger_default)
-def _test_logger(a, b=0, c=1):
+@log_exception(logger_default)
+# @log_event(logger_default)
+def _test_logger(a, b=1, c=1):
+    """testing the logger for exception"""
     return a/b
 
 
 if __name__ == "__main__":
-    print(_test_logger(1, b=0))
+    print(_test_logger.__name__)
+    print(_test_logger.__doc__)
+    print(f"no exception test:\n\t{_test_logger(1, b=1)}")
+    print(f"exception test:\n\t{_test_logger(1, b=0)}")
