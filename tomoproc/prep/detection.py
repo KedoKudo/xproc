955a14cf8dded499eaea275068393f5a545129ea
Chen <zdms1987@hotmail.com>
2020-02-11 10:12:08 -0600

    skip histogram matching

--- tomoproc/prep/detection.py
+++ tomoproc/prep/detection.py
@@ -540,7 +540,8 @@ def get_pin_rotation_center(
         pixel position of the second peak center
     """
     # only work for horizontal pin for now, should be easy to adapt to vertical pin
-    _prof = np.average(img_0, axis=0) - np.average(match_histograms(img_180, img_0), axis=0)
+    # _prof = np.average(img_0, axis=0) - np.average(match_histograms(img_180, img_0), axis=0)
+    _prof = np.average(img_0, axis=0) - np.average(img_180, axis=0)
     # fit a two peak profile
     mod = GaussianModel(prefix='p1_') + GaussianModel(prefix='p2_')
     out = mod.fit(_prof, x=np.arange(_prof.shape[0]), 
