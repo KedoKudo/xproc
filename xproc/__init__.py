688b700658a336e953b00eb2a0df3f3b032abf92
Chen <zdms1987@hotmail.com>
2020-07-22 12:56:30 -0500

    add package file

---
+++ xproc/__init__.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------
+# :author:  Chen Zhang (Kedokudo)
+# :email:   kedokudo@protonmail.com
+# 
+# Data processing toolkit for HT-HEDM instrument at Advanced Photon Source
+#
+# ----------------------------------------------------------------------
+
+__project__ = u'xproc'
